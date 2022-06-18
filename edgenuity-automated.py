b2120ee2b0aa59d0591ec2cbf5cd24da6022a6d1
Rohin Sood <rs.rohinsood@gmail.com>
2022-06-17 23:43:00 -0700

    find functions

--- edgenuity-automated.py
+++ edgenuity-automated.py
@@ -1,12 +1,43 @@
-# selenium 4
+from distutils.command.build import build
+from itertools import dropwhile
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
 
-driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()))
+ch_options=webdriver.ChromeOptions()
+ch_options.add_experimental_option("detach", True)
+ch_options.add_argument('log-level=3')
 
-driver.get("https://google.com")
+driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), options=ch_options)
 
-print (driver.title)
+driver.get("https://launchpad.classlink.com/poway")
 
-driver.quit()
+wait = WebDriverWait(driver=driver, timeout=10)
+
+def findElement( finder: By, element: str ):
+    return driver.find_element(finder, element)
+    
+def findElements( finder: By, element: str ):
+    return driver.find_elements(finder, element)    
+
+def waitFindElement( finder: By, element: str):
+    wait.until(EC.element_to_be_clickable(
+            (finder, element)
+        )
+    )
+    
+    return driver.find_element(finder, element)
+
+def waitFindElements( finder: By, element: str):
+    wait.until(EC.element_to_be_clickable(
+            (finder, element)
+        )
+    )
+    
+    return driver.find_elements(finder, element)
+
