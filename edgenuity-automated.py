1d8134d7f58533d84fcb0e558822633f972cf1d3
Rohin Sood <rs.rohinsood@gmail.com>
2022-06-19 19:28:58 -0700

    remove unused imports

--- edgenuity-automated.py
+++ edgenuity-automated.py
@@ -1,6 +1,4 @@
-from distutils.command.build import build
 from itertools import dropwhile
-from socket import timeout
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
@@ -9,7 +7,6 @@ from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
-import time as real_time
 from selenium.common.exceptions import TimeoutException
 
 ch_options=webdriver.ChromeOptions()
