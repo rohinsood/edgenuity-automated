70b236d13a142e489fbe1788c5dc4faa216492ed
Rohin Sood <rs.rohinsood@gmail.com>
2022-06-22 17:16:56 -0700

    big boy commit: iframe switching fix stopped reading the video lenght, just checking for opacity change of the next frame button (bruh) waitForOpacityChange() funciton in Utils.pyy

--- Edgenuity/Edgenuity.py
+++ Edgenuity/Edgenuity.py
@@ -18,12 +18,11 @@ def nextActivity():
 
     print("Going to the next activity")
 
-    iFrame = waitFindElement(By.XPATH, '//iframe[@allowtransparency="true"]', implicit_wait=10)
-    driver.switch_to.frame(iFrame)
-    
-    print("Switching to iframe")
-
 def completeActivity():
+    print("Switching to iframe")
+    stage_iframe = waitFindElement(By.XPATH, '//iframe[@id="stageFrame"]')
+    driver.switch_to.frame(stage_iframe)
+
     total_frames = waitFindElements(By.XPATH, '//ol[@class="FramesList"]/li')
     activity_frames = total_frames
     activity_frames.pop(0)
@@ -36,113 +35,78 @@ def completeActivity():
         if ((frame.get_attribute("class") == "FrameCurrent")):
 
             print("Finding current frame")
-            upcoming_frames = list(dropwhile( lambda completedFrame: completedFrame != frame, total_frames ))
+
+            upcoming_frames = list(dropwhile( lambda completedFrame: completedFrame != frame, activity_frames ))
             break
         elif ((frame.get_attribute("class") == "FrameCurrent FrameComplete")):
             
             print("Current frame is already complete")
+
             next_frame = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a')
             next_frame.click()
             continue
     
     for frame in upcoming_frames:
         try:
-
-            video = waitFindElement(By.XPATH, '//video[@id="home_video_js"]')
-
-            hover = ActionChains(driver=driver)
-            hover.move_to_element(video)
-
-            hover.perform()
-            try:
-                paused = waitFindElement(By.XPATH, '//li[@class="play"]')
-                paused.click()
-
-            except TimeoutException:
-                pass
-
-            video_timer = (waitFindElement(By.XPATH, '//li[@id="uid1_time"]').text).split(" / ")
-            time_passed_ms = video_timer[0]
-            video_length_ms = video_timer[1]
-
-            video_length_ms = video_length_ms.split(":")
-            video_length_s = (int(video_length_ms[0])*60) + int(video_length_ms[1])
             
-            time_passed_ms = time_passed_ms.split(":")
-            time_passed_s = (int(time_passed_ms[0])*60) + int(time_passed_ms[1])
+            print("Checking for question")
 
-            count = 0
-            time_passed_log = []
+            video_iframe = waitFindElement(By.XPATH, '//iframe[@id="iFramePreview"]')
+            driver.switch_to.frame(video_iframe)
+            waitFindElement(By.CLASS_NAME, 'title-bar')
 
-            print("Waiting for video to finish")
+            print("Question Detected!")
 
-            while((time_passed_s != video_length_s)):
-                
-                video_timer = waitFindElement(By.XPATH, '//li[@class="timer"]')
-                
-                video_timer = video_timer.text.split(" / ")
-                time_passed_ms = (video_timer[0]).split(":")
-
-                time_passed_s = (int(time_passed_ms[0])*60) + int(time_passed_ms[1])
-                
-                count += 1
-                time_passed_log.append(time_passed_s)
-
-                if(count == 3):
-                    
-                    time_passed_check = (time_passed_log[0] == time_passed_log[2]) and (time_passed_log[0] == time_passed_log[1]) and (time_passed_log[1] == time_passed_log[2]) and ((time_passed_log[0]+1) == video_length_s)
-
-                    if(time_passed_check):
-                        break
-                    
-                    time_passed_log = []
-                    count = 0
-
-            print("Video finished!")
-
-            if (frame == upcoming_frames[( len(upcoming_frames)-1 )]):
-
-                print("Last frame!")
-                next_frame = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a')
-                next_frame.click()
-                break
-
-            else: 
-
-                print("Next frame!")
-                next_frame = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a')
-                next_frame.click()
-                continue
-
-        except TimeoutException:
+            driver.switch_to.default_content()
+            driver.switch_to.frame(stage_iframe)
 
             driver.execute_script("alert('You are on a question frame!');")
-            print("Question Frame!")
-
+            
+            print("Alerted!")
+            
             while True:
                 try:
                     driver.switch_to.alert
                 except:
                     break
             
-            print("Waiting for question to be marked as complete")
-            next_frame_ready = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a').get_attribute("style")
-            while (next_frame_ready == "opacity: 1;"):
-                next_frame_ready = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a').get_attribute("style")
+            waitForOpacityChange(By.XPATH, '//li[@class="FrameRight"]')
             
+            print("Question complete!")
+
             if (frame == upcoming_frames[( len(upcoming_frames)-1 )]):
                 
                 print("Last frame!")
-                next_frame = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a')
-                next_frame.click()
                 break
 
             else: 
 
                 print("Next frame!")
-                next_frame = waitFindElement(By.XPATH, '//li[@class="FrameRight"]/a')
-                next_frame.click()
                 continue
     
+
+        except TimeoutException:
+
+            print("Video Detected!")
+
+            driver.switch_to.default_content()
+            driver.switch_to.frame(stage_iframe)
+
+            waitForOpacityChange(By.XPATH, '//li[@class="FrameRight"]')
+
+            print("Video finished!")
+
+            if (frame == upcoming_frames[( len(upcoming_frames)-1 )]):
+                
+                print("Last frame!")
+                break
+
+            else: 
+                
+                print("Next frame!")
+                continue
+
+            
     print("Activity successfully completed!")
+
     driver.quit()
--- Utils.py
+++ Utils.py
@@ -8,10 +8,12 @@ from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import TimeoutException
+from selenium.common.exceptions import ElementClickInterceptedException
+from selenium.common.exceptions import NoSuchElementException
 
 chrome_options=webdriver.ChromeOptions()
 chrome_options.add_experimental_option("detach", True)
-chrome_options.add_argument("--window-size=840,640")
+chrome_options.add_argument("--window-size=980,1080")
 chrome_options.add_argument('log-level=3')
 chrome_options.add_argument('−−mute−audio')
 # chrome_options.add_argument("--headless")
@@ -20,28 +22,55 @@ driver = webdriver.Chrome(service=Service(ChromeDriverManager().install()), opti
 
 wait = WebDriverWait(driver=driver, timeout=5)
 
-def findElement( finder: By, element: str ):
+def findElement( finder: By, element: str, implicit_wait=0 ):
+    driver.implicitly_wait(implicit_wait)
+
     return driver.find_element(finder, element)
     
-def findElements( finder: By, element: str ):
+def findElements( finder: By, element: str, implicit_wait=0 ):
+    driver.implicitly_wait(implicit_wait)
+
     return driver.find_elements(finder, element)    
 
-def waitFindElement( finder: By, element: str, implicit_wait=0):
+def waitFindElement( finder: By, element: str ):
     wait.until(EC.element_to_be_clickable(
             (finder, element)
         )
     )
-    
-    driver.implicitly_wait(implicit_wait)
 
     return driver.find_element(finder, element)
 
-def waitFindElements( finder: By, element: str, implicit_wait=0 ):
+def waitFindElements( finder: By, element: str ):
     wait.until(EC.element_to_be_clickable(
             (finder, element)
         )
     )
-    
-    driver.implicitly_wait(implicit_wait)
 
     return driver.find_elements(finder, element)
+
+def waitFindElementClick ( finder: By, element: str):
+    wait.until(EC.element_to_be_clickable(
+            (finder, element)
+        )
+    )
+
+    driver.find_element(finder, element).click()
+
+def waitForOpacityChange ( finder: By, element: str ):
+    opacity_element = waitFindElement(finder, element)
+    
+    print("Waiting for next button to flash")
+
+    # wait for opacity to change
+    while ((opacity_element.get_attribute('style') == "") or (opacity_element.get_attribute('style') == "opacity: 1;")):
+        try:
+            opacity_element = waitFindElement(finder, element)
+        except NoSuchElementException:
+            break
+
+    opacity_element = waitFindElement(finder, (element + "/a"))
+    opacity_element.click()
+
+    
+
+    
